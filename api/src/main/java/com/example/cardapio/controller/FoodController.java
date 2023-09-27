33915ee7f00a5113e5cb88d9be4921a292fce4e5
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-09-27 15:21:45 -0300

    Projeto de Cardapio

---
+++ api/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,38 @@
+package com.example.cardapio.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+
+@RestController //Mapeia que ela é um controller
+@RequestMapping("food") //Mapeia o request que cair no endpoint food
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @GetMapping //Bateu no resquet food usando get, esse método vai ser usado
+    public List<FoodResponseDTO> getAll() {
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
